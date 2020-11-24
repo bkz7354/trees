a71b4ce18ee28f33a67f693f7d53b93f37cd9446
arinaklushina <arina.k.iphone@gmail.com>
2020-11-24 15:33:37 +0300

    added class for AVL's, and funcs that show stuff stored inside the tree

--- AVL_tree.hpp
+++ AVL_tree.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 //
 //  main.cpp
 //  avl_tree
@@ -8,37 +6,113 @@
 //
 
 #include <iostream>
-#include <vector>
 
-//creating a structure for nodes - like in stack class
+
+
 template<typename T>
-struct node{
-    T val;//value stored in the node
-    node* l = nullptr;//pointer to daughter l
-    node* r = nullptr;//pointer to daughter r
-    unsigned char height; //height of a subtree which has this node as a root
+class avl_tree{
+private:
 
-    node(const T& val_) { val = val_; l = r = 0; height = 1; }//construct a node which stores val_ and has type T (but val_ is T&)
+    struct node{
+        T val;//value stored in the node
+        
+        node* l = nullptr;//pointer to daughter l
+        node* r = nullptr;//pointer to daughter r
+        unsigned char height = 0; //height of a subtree which has this node as a root
 
+        node(){};
+        node(const T& val_): val(val_), height{1} {}//construct a node which stores val_ and has type T (but val_ is T&)
+
+    };
+    using tree_t = node*;
+    
+    tree_t root = nullptr;
+
+    int difference(tree_t p);
+    unsigned char height(tree_t p);
+    tree_t rotateright(tree_t p);
+    tree_t rotateleft(tree_t p);
+    tree_t balance(tree_t p);
+    
+    tree_t inner_insert(tree_t p, const T& val);
+        
+    void fixheight(tree_t p);
+    void inner_show(tree_t p, int l);
+    bool inner_find(tree_t p, const T& val);
+    void inorder(tree_t t);
+    void preorder(tree_t t);
+    void postorder(tree_t t);
+    
+    tree_t findmin(tree_t p);
+    tree_t removemin(tree_t p);
+    tree_t inner_remove(tree_t p, int k);
+
+public:
+    
+    avl_tree() {}
+
+    void insert(const T& val){
+        root = inner_insert(root, val);
+    }
+
+    void remove(const T& val){
+        root = inner_remove(root, val);
+    }
+
+    void find(const T& val){
+        return inner_find(root, val);
+    }
+
+    void display()
+        {
+            inorder(root);
+            std::cout << std::endl;
+        }
+};
+
+//functions outside the class
+
+
+template<typename T>
+void avl_tree<T>::inorder(tree_t t) {
+   if (t == NULL)
+      return;
+      inorder(t->l);
+    std::cout << t->val << " ";
+      inorder(t->r);
 };
 
 template<typename T>
-using tree_t = node<T>*;//now tree_t means "type node<T>*" - a pointer to a node storing value of T
+void avl_tree<T>::preorder(tree_t t) {
+   if (t == NULL)
+      return;
+    std::cout << t->val << " ";
+      preorder(t->l);
+      preorder(t->r);
+};
+template<typename T>
+void avl_tree<T>::postorder(tree_t t) {
+   if (t == NULL)
+      return;
+      postorder(t->l);
+      postorder(t->r);
+      std::cout << t->val << " ";
+};
 
 template <typename T>
-unsigned char height(tree_t<T> p)
+unsigned char avl_tree<T>::height(tree_t p)
 {
     return p ? p->height : 0;//returns height of the node if further branches exist
 };
 
 template <typename T>
-int bfactor(tree_t<T> p)
+int avl_tree<T>::difference(tree_t p)
 {
     return height(p->r)-height(p->l);
 };
 
 template <typename T>
-void fixheight(tree_t<T> p)
+void avl_tree<T>::fixheight(tree_t p)
 {
     unsigned char hl = height(p->l);
     unsigned char hr = height(p->r);
@@ -47,41 +121,40 @@ void fixheight(tree_t<T> p)
 
 //balancing
 template <typename T>
-tree_t<T> rotateright(tree_t<T> p) // right rotation
+typename avl_tree<T>::tree_t avl_tree<T>::rotateright(tree_t p) // right rotation
 {
-    tree_t<T> q = p->left;
-    p->left = q->right;
-    q->right = p;
+    tree_t q = p->l;
+    p->l = q->r;
+    q->r = p;
     fixheight(p);//update heights
     fixheight(q);
     return q;
 };
 
 template <typename T>
-tree_t<T> rotateleft(tree_t<T> q) // left rotation
+typename avl_tree<T>::tree_t avl_tree<T>::rotateleft(tree_t q) // left rotation
 {
-    tree_t<T> p = q->right;
-    q->right = p->left;
-    p->left = q;
+    tree_t p = q->r;
+    q->r = p->l;
+    p->l = q;
     fixheight(q);
     fixheight(p);
     return p;
 };
-
 template <typename T>
-tree_t<T> balance(tree_t<T> p) // balancing node p
+typename avl_tree<T>::tree_t avl_tree<T>::balance(tree_t p) // balancing node p
 {
     fixheight(p);
-    if( bfactor(p)==2 )
+    if( difference(p)==2 )
     {
-        if( bfactor(p->right) < 0 )
-            p->right = rotateright(p->right);
+        if( difference(p->r) < 0 )
+            p->r = rotateright(p->r);
         return rotateleft(p);
     }
-    if( bfactor(p)==-2 )
+    if( difference(p)==-2 )
     {
-        if( bfactor(p->left) > 0  )
-            p->left = rotateleft(p->left);
+        if( difference(p->l) > 0  )
+            p->l = rotateleft(p->l);
         return rotateright(p);
     }
     return p; //balancing not needed
@@ -89,68 +162,73 @@ tree_t<T> balance(tree_t<T> p) // balancing node p
 
 
 template<typename T>
-tree_t<T> insert(tree_t<T> p, const T& val){
+typename avl_tree<T>::tree_t avl_tree<T>::inner_insert(tree_t p, const T& val){
     if(!p)//equiv to p == nullptr
-        return new node<T>(val);
+        return new node(val);
 
     if(val < p->val)
-        p->l = insert(p->l, val);
+        p->l = inner_insert(p->l, val);
     else if(val > p->val)
-        p->r = insert(p->r, val);
+        p->r = inner_insert(p->r, val);
 
     return balance(p);
 };
 
 template<typename T>
-bool find(tree_t<T> t, const T& val){
+bool avl_tree<T>::inner_find(tree_t t, const T& val){
     if(!t)
         return false;
 
     if(t->val == val)
         return true;
     else if(val < t->val)
-        return find(t->l, val);
+        return inner_find(t->l, val);
     else if(val > t->val)
-        return find(t->r, val);
+        return inner_find(t->r, val);
 };
-
 template<typename T>
-tree_t<T> findmin(tree_t<T> p) // searching a node w/min key p
+typename avl_tree<T>::tree_t avl_tree<T>::findmin(tree_t p) // searching a node w/min key p
 {
     return p->left ? findmin(p->left) : p;
 };
-
 template<typename T>
-tree_t<T> removemin(tree_t<T> p) // deleting the min key node  p
+typename avl_tree<T>::tree_t avl_tree<T>::removemin(tree_t p) // deleting the min key node  p
 {
     if( p->left==0 )
         return p->right;
     p->left = removemin(p->left);
     return balance(p);
 };
-
 template<typename T>
-tree_t<T> remove(tree_t<T> p, int k) // deleting key k from tree p
+typename avl_tree<T>::tree_t avl_tree<T>::inner_remove(tree_t p, int k) // deleting key k from tree p
 {
     if( !p ) return 0;
     if( k < p->key )
-        p->left = remove(p->left,k);
+        p->left = inner_remove(p->left,k);
     else if( k > p->key )
-        p->right = remove(p->right,k);
+        p->right = inner_remove(p->right,k);
     else //  k == p->key
     {
-        tree_t<T> q = p->left;
-        tree_t<T> r = p->right;
+        tree_t q = p->left;
+        tree_t r = p->right;
         delete p;
         if( !r ) return q;
-        tree_t<T> min = findmin(r);
+        tree_t min = findmin(r);
         min->right = removemin(r);
         min->left = q;
         return balance(min);
     }    return balance(p);
+};
+
+
+
+
+int main(){
+    avl_tree<int> cedre = avl_tree<int>();
+    cedre.insert(3);
+    cedre.insert(2);
+    cedre.insert(1);
+    cedre.insert(5);
+    cedre.insert(-11);
+    cedre.display();
 }
-
-
-// int main(){
-
-// }
